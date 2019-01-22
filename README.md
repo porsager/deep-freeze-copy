aa685ec59739ffb4b56d526d4ca89c3ff1b5ef9f
Rasmus Porsager <rasmus@porsager.com>
2019-01-22 23:01:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 😬🥶 `deep-freeze-copy`
+
+This is just a single function that returns a deeply frozen copy of an object.
---
+++ index.cjs.js
@@ -0,0 +1,12 @@
+module.exports = function deepFreezeCopy(o) {
+  return Object.freeze(
+    Object.keys(o).reduce((acc, key) => (
+      acc[key] = typeof o[key] === 'object' && o[key] !== null
+        ? deepFreezeCopy(o[key])
+        : o[key] instanceof Date
+          ? new Date(o[key])
+          : o[key],
+      acc
+    ), Array.isArray(o) ? [] : {})
+  )
+}
